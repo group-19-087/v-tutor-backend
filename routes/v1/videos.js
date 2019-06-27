797fa6c76fd9f7f094142f00a056bb66a5b15ebe
Isuru Hettiarachchi <isuru71@gmail.com>
2019-06-27 22:33:19 +0530

    Add multer-s3 AWS S3 upload functionality

--- routes/v1/videos.js
+++ routes/v1/videos.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var multer  = require('multer');
+var AWS = require('aws-sdk');
+var multerS3 = require('multer-s3');
+
+require('dotenv').config()
 
 var storage = multer.diskStorage({
   destination: function (request, file, callback){
@@ -9,18 +13,40 @@ var storage = multer.diskStorage({
   filename: function(request, file, callback){
       callback(null, file.originalname);
   }
-}); 
+});
 
-var upload = multer({ storage: storage })
+let s3bucket = new AWS.S3({
+  accessKeyId: process.env.IAM_USER_KEY,
+  secretAccessKey: process.env.IAM_USER_SECRET,
+  region: process.env.AWS_REGION
+});
+
+var upload = multer({
+  storage: multerS3({
+    s3: s3bucket,
+    bucket: process.env.BUCKET_NAME,
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    acl: 'public-read',
+    metadata: function(req, file, cb) {
+      cb (null, {fieldName: file.fieldname});
+    },
+    key: function(req, file, cb) {
+      cb (null, file.originalname); 
+    }
+  })
+});
+
+
 
 router.get('/', function(req, res, next) {
   res.send('route to get video resources');
 });
 
 router.post('/upload', upload.single('file'), function (req, res, next) {
-  // req.file is the file
-  console.log(req.file);
-  res.send('Your file' + req.file.originalname + ' was uploaded'); 
+  
+
+  res.send('Successfully uploaded ' + req.file.length + ' files!'); 
+  
 })
 
 
