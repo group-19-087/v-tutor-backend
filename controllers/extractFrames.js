a11c957f252715b3d4800f168bd16d8f01a4110e
dammakaru <dammakaru@gmail.com>
2019-07-17 11:26:49 +0530

    Add frame extraction code

--- controllers/extractFrames.js
+++ controllers/extractFrames.js
@@ -5,79 +5,45 @@ const spawn = require('child_process').spawn;
 const s3 = new AWS.S3();
 
 module.exports.extract = function (bucket, key) {
-    const params = { Bucket: bucket , Key: key };
+    const params = { Bucket: bucket, Key: key };
     const url = s3.getSignedUrl('getObject', params);
-    const tmpDirectory = '~/tmp/frames';
+    const tmpDirectory = __dirname + '/tmp/frames';
     console.log('The URL is', url);
+    console.log('tmpDirectory : ' + `${tmpDirectory}/frame-%04d.jpg`);
 
     return new Promise((resolve, reject) => {
-        if (!fs.existsSync(tmpDirectory)){
+        if (!fs.existsSync(tmpDirectory)) {
             fs.mkdirSync(tmpDirectory);
-            fs.chmod( tmpDirectory, '755', function(err){
-                if(err){
+            fs.chmod(tmpDirectory, '755', function (err) {
+                if (err) {
                     console.log(err);
                     throw err;
                 }
                 console.log('Created directory');
             });
-            resolve('created directory');
         } else {
             console.log('directory exists');
-            resolve('directory exists');
         }
 
-          // const ffmpeg = spawn('ffmpeg', [
-    //     '-i', 
-    //     `${ url }`, 
-    //     '-c:a', 
-    //     'aac', 
-    //     `media/${ fileName }`
-    // ]);
-
-    // ffmpeg.on('exit', (statusCode) => {
-    // if (statusCode === 0) {
-    //     console.log('conversion successful')
-    // }
-    // })
-
-    // ffmpeg.stderr.on('data', (err) => {
-    //     console.log('err:', new String(err))
-    // })
-    })
-}
-
-function extractFrames(){
-    console.log(target);
-    var dir = '/tmp/frames';
-    return new Promise((resolve, reject) => {
-        if (!fs.existsSync(dir)){
-            fs.mkdirSync(dir);
-            fs.chmod('/tmp/frames', '755', function(err){
-                if(err){
-                    console.log(err);
-                }
-                console.log("Created directory");
-            });
-        }
         const ffmpeg = spawn('ffmpeg', [
             '-i',
-            target,
+            url,
             '-vf',
-            'fps=1',
-            '/tmp/frames/frame-%04d.jpg',
+            'fps=0.5',
+            `${tmpDirectory}/frame-%04d.jpg`,
             '-hide_banner'
         ]);
-        
+
         ffmpeg.on('exit', (statusCode) => {
             if (statusCode === 0) {
                 console.log('Frames extracted');
                 resolve('Frames extracted');
             }
         });
-        
+
         ffmpeg.stderr.on('data', (err) => {
             console.log(err);
             //reject(err);
         });
-    });
+    })
 }
