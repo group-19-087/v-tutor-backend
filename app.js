fe25c740b447d5616fa604e2b355b0ef4f6d24f3
dammakaru <dammakaru@gmail.com>
2019-07-26 18:04:31 +0530

    Use MongoDB Atlas

--- app.js
+++ app.js
@@ -37,7 +37,12 @@ app.use(function(req, res, next) {
 var mongoUser = process.env.MONGO_USER;
 var mongoPass = process.env.MONGO_PASSWORD;
 
-mongoose.connect('mongodb://'+ mongoUser +':'+ mongoPass +'@ds151066.mlab.com:51066/cdap-19-087', {useNewUrlParser: true})
+mongoose.connect('mongodb://'+ mongoUser +':'+ mongoPass +
+    '@cluster0-shard-00-00-phute.mongodb.net:27017,'+
+    'cluster0-shard-00-01-phute.mongodb.net:27017,'+
+    'cluster0-shard-00-02-phute.mongodb.net:27017/'+
+    'test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', 
+{useNewUrlParser: true})
     .catch((err) => {
         console.log('Unable to connect to mongoose instance ' + err);
     });
