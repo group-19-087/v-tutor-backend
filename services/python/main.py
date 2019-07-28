2caaaa28f0df55b583cd9e31230293b6b032f186
dammakaru <dammakaru@gmail.com>
2019-07-28 16:21:51 +0530

    Add OCR script call to flow

--- services/python/main.py
+++ services/python/main.py
@@ -6,7 +6,7 @@ import ocr
 ap = argparse.ArgumentParser()
 ap.add_argument("--image_path",
                 help="path to input images to be OCR'd", 
-                default="/home/ubuntu/v-tutor-backend/v-tutor-backend/frames")
+                default="/home/ubuntu/v-tutor-backend/v-tutor-backend/frames/extracted/")
 ap.add_argument("-p", "--preprocess", type=str,
                 help="type of preprocessing to be done", default="thresh")
 args = vars(ap.parse_args())
