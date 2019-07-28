29a5919d52ef980605538855361cd1c145ff6ee9
dammakaru <dammakaru@gmail.com>
2019-07-28 16:57:11 +0530

    Delete invalid time object

--- services/python/main.py
+++ services/python/main.py
@@ -1,6 +1,4 @@
 import argparse
-import time
-
 import ocr
 
 ap = argparse.ArgumentParser()
@@ -11,10 +9,6 @@ ap.add_argument("-p", "--preprocess", type=str,
                 help="type of preprocessing to be done", default="thresh")
 args = vars(ap.parse_args())
 
-start = time.perf_counter()
 ocr.run_ocr(args["image_path"], args["preprocess"])
-ocr_time = time.perf_counter() - start
 
-print("\n")
-print("OCR completed in : ", ocr_time)
 print("\n")
