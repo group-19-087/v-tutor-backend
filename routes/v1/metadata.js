702f1c84670d26d7afbc640411d7b85803fc6691
dammakaru <dammakaru@gmail.com>
2019-07-31 15:28:01 +0530

    Allow project through request header

--- routes/v1/metadata.js
+++ routes/v1/metadata.js
@@ -4,12 +4,12 @@ const metadataService = require('../../services/metadata.service');
 
 router.get('/:id', async (request, response) => {
     try {
-        const projection = "videoTitle description";
-        const data = await metadataService.findMetaDataById(request.params.id, projection);
+        const projectionValues = request.header('cdap-projection-values');
+        const data = await metadataService.findMetaDataById(request.params.id, projectionValues);
         response.send(data);
     } catch (error) {
         response.status(500).send(error);
     }
-})
+});
 
 module.exports = router;
