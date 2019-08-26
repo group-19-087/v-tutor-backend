63b69e75d578db066af39925934c7d0fe8beaf35
Stefinie fernando <minolispencer@gmail.com>
2019-08-26 22:08:44 +0530

    Update topics endpoint modification

--- routes/v1/videos.js
+++ routes/v1/videos.js
@@ -193,9 +193,9 @@ router.put('/update-comments/:id', function (req, res) {
 
 router.put('/update-topics/:id', function (req, res) {
     metaDataService.updateTopics(req.params.id, req.body).then(function (data) {
-        res.status(data.status).send(data.message);
+        res.status(data.status).send(data);
     }).catch(function (err) {
-        res.status(err.status).send(err.message);
+        res.status(err.status).send(err);
     });
 })
 
