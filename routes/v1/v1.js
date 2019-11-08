3f797d7eb92d2c353f4d91d07f60c55740b616ee
dammakaru <dammakaru@gmail.com>
2019-11-08 21:03:26 +0530

    Make routing endpoint for testing job queue

--- routes/v1/v1.js
+++ routes/v1/v1.js
@@ -5,10 +5,12 @@ var videoRouter = require('./videos')
 var userRouter = require('./user')
 var moduleRouter = require('./modules')
 var metaDataRouter = require('./metadata')
+var jobsRouter = require('./jobs')
 
 router.use('/videos', videoRouter)
 router.use('/user', userRouter)
 router.use('/modules', moduleRouter)
 router.use('/metadata', metaDataRouter)
+router.use('/jobs', jobsRouter)
 
 module.exports = router
