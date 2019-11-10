e3b4e5d116f457e19d541f11d3d4fee8ab0db186
dammakaru <dammakaru@gmail.com>
2019-11-10 15:21:15 +0530

    Add switch to slidea and code matching promises

--- services/codematch.service.js
+++ services/codematch.service.js
@@ -1,30 +1,34 @@
 const fs = require('fs')
 const spawn = require('child_process').spawn
 
-module.exports.runCodeMatching = function() {
+module.exports.runCodeMatching = function (enabled) {
   return new Promise((resolve, reject) => {
 
-    pathToScript = __dirname + "/python/codematching/main.py"
-    const matchScript = spawn('python', [pathToScript])
+    if (enabled) {
+      pathToScript = __dirname + "/python/codematching/main.py"
+      const matchScript = spawn('python', [pathToScript])
 
-    let output = '';
+      let output = '';
 
-    matchScript.on('exit', (statusCode) => {
-      if (statusCode === 0) {
-        console.log('Code Matching Script executed successfully \n')
-        resolve(output)
-      } else {
-        console.log('Non zero exit code : ' + statusCode)
-        reject('Non zero status code')
-      }
-    })
+      matchScript.on('exit', (statusCode) => {
+        if (statusCode === 0) {
+          console.log('Code Matching Script executed successfully \n')
+          resolve(output)
+        } else {
+          console.log('Non zero exit code : ' + statusCode)
+          reject('Non zero status code')
+        }
+      })
 
-    matchScript.stdout.on('data', (data) => {
-      output += data.toString()
-    })
+      matchScript.stdout.on('data', (data) => {
+        output += data.toString()
+      })
 
-    matchScript.stderr.on('data', (err) => {
-      console.log('Error : ' + err)
-    })
+      matchScript.stderr.on('data', (err) => {
+        console.log('Error : ' + err)
+      })
+    } else {
+      resolve('Code Matching not enabled')
+    }
   })
 }
