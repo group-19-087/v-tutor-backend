a5effd3230fce68545d57427cd9da08e06f31d65
sachiniepa <sachi.epa96@gmail.com>
2019-11-12 14:16:16 +0530

    Updating slide-matching service

--- services/slide-matching.service.js
+++ services/slide-matching.service.js
@@ -8,11 +8,12 @@ module.exports.slideMatching = function (response) {
         if (response.exists) {
             const pathToScript = __dirname + "/python/slide-matching/slide-matching.py"
             const id = response.contents[0].Key.slice(response.content[0].Key.lastIndexOf('/') + 1);
-            const pathToS3 = "https://cdap-19-087-vtutor-lecturematerials.s3.ap-south-1.amazonaws.com/" + id + "Slides.pdf";
-            const smScript = spawn('python', [pathToScript], pathToS3, id)
+            const lectureSlides = downloadFile('/home/', 'cdap-19-087-vtutor-lecturematerials', id + 'slides.pdf')
+            //const pathToS3 = "https://cdap-19-087-vtutor-lecturematerials.s3.ap-south-1.amazonaws.com/" + id + "Slides.pdf";
+            const smScript = spawn('python', [pathToScript], lectureSlides, id);
             smScript.on('exit', (statusCode) => {
                 if (statusCode === 0) {
-                    console.log('Slide Matching Script exited successfully with code 0')
+                    console.log('Slide Matching Script exited successfully with code 0');
                     resolve(true)
                 } else {
                     console.log('Non zero exit code : ' + statusCode)
@@ -29,3 +30,17 @@ module.exports.slideMatching = function (response) {
         }
     })
 }
+
+const downloadFile = (filePath, bucketName, key) => {
+
+    const params = {
+        Bucket: bucketName,
+        Key: key
+    };
+
+    s3.getObject(params, (err, data) => {
+        if (err) console.error(err);
+        fs.writeFileSync(filePath, data.Body.toString());
+        console.log(`${filePath} has been created!`);
+    });
+};
