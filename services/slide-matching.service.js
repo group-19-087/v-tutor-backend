9b2d8d3f7969c23910b8458fb37cf48e6a147e91
sachiniepa <sachi.epa96@gmail.com>
2019-11-12 13:20:25 +0530

    Updating slide-matching service

--- services/slide-matching.service.js
+++ services/slide-matching.service.js
@@ -6,9 +6,10 @@ module.exports.slideMatching = function (response) {
     return new Promise((resolve, reject) => {
 
         if (response.exists) {
-            pathToScript = __dirname + "/python/slide-matching/slide-matching.py"
-            const smScript = spawn('python', [pathToScript])
-
+            const pathToScript = __dirname + "/python/slide-matching/slide-matching.py"
+            const id = response.contents[0].Key.slice(response.content[0].Key.lastIndexOf('/') + 1);
+            const pathToS3 = "https://cdap-19-087-vtutor-lecturematerials.s3.ap-south-1.amazonaws.com/" + id + "Slides.pdf";
+            const smScript = spawn('python', [pathToScript], pathToS3, id)
             smScript.on('exit', (statusCode) => {
                 if (statusCode === 0) {
                     console.log('Slide Matching Script exited successfully with code 0')
