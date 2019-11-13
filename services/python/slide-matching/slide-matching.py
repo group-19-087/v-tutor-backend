a6b1423444cf6c9f251178f0fd8df9988585ec0a
sachiniepa <sachi.epa96@gmail.com>
2019-11-14 04:40:30 +0530

    Updating slide-matching algorithm

--- services/python/slide-matching/slide-matching.py
+++ services/python/slide-matching/slide-matching.py
@@ -1,19 +1,18 @@
-import pdf2image
-from PIL import Image
-import time
-import boto3
-import os, os.path
-import sys
-import glob
-import socket
-import win32com.client
 import requests
 import cv2
 import numpy as np
 import numpy
 import json
+import sys
+import time
+from pdf2image import convert_from_path
+import boto3
+import os
+# import ConfigParser
 
-socket.getaddrinfo('127.0.0.1', 8080)
+
+
+# socket.getaddrinfo('127.0.0.1', 8080)
 # DECLARE CONSTANTS
 # PDF_PATH = "C:\\Users\\HP\\Desktop\\Research\\Lecture-05-2018-v1.pdf"
 DPI = 200
@@ -27,69 +26,57 @@ USE_CROPBOX = False
 STRICT = False
 slides = []
 
-# This method converts the pdf into a set of images.
-def pdftopil(f):
+# config = ConfigParser.ConfigParser()
+# config.readfp(open(r'config'))
 
-    start_time = time.time()
-    pil_images = pdf2image.convert_from_path(f, dpi=DPI, output_folder=OUTPUT_FOLDER, first_page=FIRST_PAGE,
-                                             last_page=LAST_PAGE, fmt=FORMAT, thread_count=THREAD_COUNT, userpw=USERPWD,
-                                             use_cropbox=USE_CROPBOX, strict=STRICT)
-    print("Time taken : " + str(time.time() - start_time))
-    save_images(pil_images)
+aws_access_key_id = "AKIA5BI4TMPGSXRAMUQ6"
+aws_secret_access_key = "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"
 
+url = sys.argv[1]
+myfile = requests.get(url)
+open('slides/slides.pdf', 'wb').write(myfile.content)
 
-# This method converts a .ppt/.pptx into a pdf file
-def convert(files, formatType=32):
-    powerpoint = win32com.client.Dispatch("Powerpoint.Application")
-    print('test1')
-    # powerpoint.Interactive = 0
-    # powerpoint.Visible = 1
-    print('test2')
-    for filename in files:
-        print('test3')
-        newname = os.path.splitext(filename)[0] + ".pdf"
-        deck = powerpoint.Presentations.Open(filename)
-        print('Filename: ' + filename + 'Newname:' + newname)
-        print(formatType)
-        # powerpoint.Presentations.Close()
-        deck.SaveAs(newname, formatType)
-        # deck.Close()
-    # powerpoint.Quit()
+start_time = time.time()
+pil_images = convert_from_path('slides/slides.pdf', dpi=DPI, output_folder=OUTPUT_FOLDER, first_page=FIRST_PAGE,
+                                            last_page=LAST_PAGE, fmt=FORMAT, thread_count=THREAD_COUNT, userpw=USERPWD,
+                                            use_cropbox=USE_CROPBOX, strict=STRICT)
+print("Time taken : " + str(time.time()))
 
+count = 0
+slides = []
+for page in pil_images:
+    count = count +1
+    # Saving the image file in the filesystem
+    page.save('out-{}.jpg'.format(count), 'JPEG')
 
-# This method stores the individual slides on an s3 bucket.
-def save_images(pil_images):
-    # This method helps in converting the images in PIL Image file format to the required image format
-    s3 = boto3.client('s3')
-    index = 1
-    Slides = []
-    for image in pil_images:
-        image.save("Slides/Slide " + str(index) + ".png")
-        # s3.put_object(Body = 'image12.png', Bucket = 'cdap-slides-bucket', Key = 'folder/{}'.format('frame' + str(index) + '.jpg'))
-        # s3.upload_file('Slides/Slides ' + str(index) + '.png', 'cdap-slides-bucket', 'Lecture5/{}'.format('frame' + str(index) + '.jpg'))
-        index += 1
-    dir = 'Slides'
-    slide_count = len([name for name in os.listdir(dir) if os.path.join(dir, name)])
-    j = 1
-    while j <= slide_count:
-        s3.upload_file('Slides/Slide ' + str(j) + '.png', 'cdap-slides-bucket',
-                       sys.argv[2].format('Slide' + str(j) + '.jpg'))
-        timestamp = findTimestamp('Slides/Slide ' + str(j) + '.png')
-        location = boto3.client('s3').get_bucket_location(Bucket='cdap-slides-bucket')['LocationConstraint']
-        #url = "https://s3-%s.amazonaws.com/%s/%s" % (location, 'cdap-slides-bucket', key)
-        url = "https://%s.s3.%s.amazonaws.com/%s" % ('cdap-slides-bucket', location, 'Slide' + str(index) + '.jpg')
-        slides.append({"slide": url, "timestamp": timestamp})
-        # r2 = requests.put('http://localhost:3000/v1/metadata/updateTimestamp/', params=timestamp)
-        j += 1
+    # Uploading to S3
+    session = boto3.Session(aws_access_key_id=aws_access_key_id,aws_secret_access_key=aws_secret_access_key)
+    s3 = session.resource('s3')
+    s3.meta.client.upload_file(Filename='out-{}.jpg'.format(count), Bucket='cdap-slides-bucket', Key='{}/slide-{}.jpg'.format(sys.argv[2],count), ExtraArgs={'ACL':'public-read'})
+    # Matching frames with slides
+    image = cv2.imread('out-{}.jpg'.format(count))
+    height = np.size(image, 0)
+    width = np.size(image, 1)
+    imagesize = height + "x" + width
+    command = "ffmpeg -i "+ sys.argv[2] + "-s " + imagesize + " -vf fps=1 frame/frame%04.jpg -hide_banner"
+    timestamp = findTimestamp('out-{}.jpg'.format(count))
+    print(timestamp)
+    s3_url = "https://cdap-slides-bucket.s3.ap-south-1.amazonaws.com/{}/slide-{}.jpg".format(sys.argv[2], count)
+    slides.append({"slide": s3_url, "timestamp":timestamp})
+    os.remove('out-{}.jpg'.format(count))
 
-    r1 = requests.put('http://localhost:3000/v1/metadata/updateSlides/%s' % sys.argv[2] , data=json.dump({"slides": slides}))
-    # b = pickle.dumps(Slides)
-    # s3.put_object(Body=Slides)
+request = requests.put('http://localhost:3000/v1/metadata/slides/{}'.format(sys.argv[2]), data=json.dumps({"slides": slides}))
+if request.status_code == 200:
+    print("DB updated")
+else:
+    print("Could not update DB")
+
+# save_images(pil_images)
 
 
 # This method returns the occurence of a given slide(in seconds)
 def findTimestamp(slide):
-    path, dirs, files = next(os.walk("/home/ubuntu/v-tutor-backend/v-tutor-backend/frames/extracted/"))
+    path, dirs, files = next(os.walk("C:/Users/HP/Desktop/Research/NUWAN SIR LEC VIDS/OOC Lecture Videos/test3/rame"))
     file_count = len(files)
     print(file_count)
     i = "00001"
@@ -99,7 +86,7 @@ def findTimestamp(slide):
         print(str(img1.shape[0]) + "," + str(img1.shape[1]))
         # print('frames/frame'+ i +'.jpg')
         # getting the difference of the images
-        img2 = cv2.imread('frames/frame' + i + '.jpg', 0)
+        img2 = cv2.imread('./frames/frame' + i + '.jpg', 0)
         # print(img2.shape[0] + ", " + img2.shape[1]);
         # resizing the images
         print(str(img2.shape[0]) + "," + str(img2.shape[1]))
@@ -119,7 +106,7 @@ def findTimestamp(slide):
             print(i)
 
         timestamp = getTimeInSeconds(i)
-        request = requests.put('http://localhost:3000/v1/metadata/updateTimestamp/', params=timestamp)
+        # request = requests.put('http://localhost:3000/v1/metadata/updateTimestamp/', params=timestamp)
         return timestamp
 
 
@@ -137,6 +124,3 @@ def getTimeInSeconds(filename):
         i=i+1
     return time
 
-
-if __name__ == "__main__":
-    pdftopil(sys.argv[0])
