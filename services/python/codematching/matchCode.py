2effd918635bf21e9c3cda381232833aaedb0ca9
dammakaru <dammakaru@gmail.com>
2019-11-13 23:38:18 +0530

    Check case where no code is present

--- services/python/codematching/matchCode.py
+++ services/python/codematching/matchCode.py
@@ -24,47 +24,51 @@ def match(path_to_code_file, path_to_frames, title):
         "lines":[]
         }
 
-    # lines_in_source_code.reverse()
-    frames = os.listdir(path_to_frames)
-    sorted_frames = sorted(frames, reverse = True) # read frames in reverse
+    if len(os.listdir(path_to_frames)) == 0:
+        print json.dumps(data)
+    else:    
+        print("Directory is not empty")
+        # lines_in_source_code.reverse()
+        frames = os.listdir(path_to_frames)
+        sorted_frames = sorted(frames, reverse = True) # read frames in reverse
 
-    for idx, sc_line in enumerate(lines_in_source_code):
-        # print("LINE: ", idx, " : ", sc_line[0:10])
-        max_ratio = 0.0
-        cur_ratio = 0.0
-        max_ratio_frame = ""
-        if len(sc_line) < 3:
-            # print("continue : ", sc_line)
-            continue
+        for idx, sc_line in enumerate(lines_in_source_code):
+            # print("LINE: ", idx, " : ", sc_line[0:10])
+            max_ratio = 0.0
+            cur_ratio = 0.0
+            max_ratio_frame = ""
+            if len(sc_line) < 3:
+                # print("continue : ", sc_line)
+                continue
 
-        for file in sorted_frames:
-            path_to_File = os.path.join(path_to_frames, file)
-            with open(path_to_File) as f:
-                for i, line in enumerate(f):
-                    cur_ratio = SequenceMatcher(None, sc_line, line).ratio()
-                    if(cur_ratio >= max_ratio):
-                        max_ratio = cur_ratio
-                        max_ratio_frame = file
-                    # # print("line ", i, " ratio: ", SequenceMatcher(None, sc_line, line).quick_ratio())
-                    # print("line ", i, " ratio: ", cur_ratio)
+            for file in sorted_frames:
+                path_to_File = os.path.join(path_to_frames, file)
+                with open(path_to_File) as f:
+                    for i, line in enumerate(f):
+                        cur_ratio = SequenceMatcher(None, sc_line, line).ratio()
+                        if(cur_ratio >= max_ratio):
+                            max_ratio = cur_ratio
+                            max_ratio_frame = file
+                        # # print("line ", i, " ratio: ", SequenceMatcher(None, sc_line, line).quick_ratio())
+                        # print("line ", i, " ratio: ", cur_ratio)
 
-        frame_number = int(max_ratio_frame.split(".")[0])
-        seconds = frame_number * 2 - 2 # calculated with [1/(fps that the video is sampled at)] ie. 1/0.2
+            frame_number = int(max_ratio_frame.split(".")[0])
+            seconds = frame_number * 2 - 2 # calculated with [1/(fps that the video is sampled at)] ie. 1/0.2
 
-        if seconds < 0:
-            seconds = 0
+            if seconds < 0:
+                seconds = 0
 
-        h, m, s = extract_timestamp(seconds)
-        timestamp = '{:d}:{:02d}:{:02d}'.format(h, m, s)
+            h, m, s = extract_timestamp(seconds)
+            timestamp = '{:d}:{:02d}:{:02d}'.format(h, m, s)
 
-        line = {
-            "content": sc_line,
-            "timestamp": timestamp,
-            "seconds": seconds
-        }
-        data["lines"].append(line)
+            line = {
+                "content": sc_line,
+                "timestamp": timestamp,
+                "seconds": seconds
+            }
+            data["lines"].append(line)
 
-    print json.dumps(data)
-    ocr_output_folder = "/home/ubuntu/v-tutor-backend/v-tutor-backend/ocroutput"
-    # shutil.rmtree(ocr_output_folder)
+        print json.dumps(data)
+        ocr_output_folder = "/home/ubuntu/v-tutor-backend/v-tutor-backend/ocroutput"
+        shutil.rmtree(ocr_output_folder)
 # ============================================================================================
